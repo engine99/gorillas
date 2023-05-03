447dc2e34e5e161c45fe6c0a70371b7ed8eb67a9
trout_wmd <darrentmacdonald@gmail.com>
2023-05-03 10:51:32 -0300

    Adding tailwindcss, adding space at bottom to help mobile view

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/backend/views/*",
+    "./src/frontend/**/*"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
